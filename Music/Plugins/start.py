a2236784a5db928f1061aef2c089a9afa6f2d322
Percy <percy@railway.app>
2021-12-16 15:16:00 +0000

    Initial Commit

---
+++ Music/Plugins/start.py
@@ -0,0 +1,102 @@
+import asyncio
+from time import time
+from datetime import datetime
+from Music import BOT_USERNAME
+from Music.config import UPDATES_CHANNEL, ZAID_SUPPORT
+from Music.MusicUtilities.helpers.filters import command
+from Music.MusicUtilities.helpers.command import commandpro
+from pyrogram import Client, filters
+from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
+
+
+START_TIME = datetime.utcnow()
+START_TIME_ISO = START_TIME.replace(microsecond=0).isoformat()
+TIME_DURATION_UNITS = (
+    ('week', 60 * 60 * 24 * 7),
+    ('day', 60 * 60 * 24),
+    ('hour', 60 * 60),
+    ('min', 60),
+    ('sec', 1)
+)
+
+async def _human_time_duration(seconds):
+    if seconds == 0:
+        return 'inf'
+    parts = []
+    for unit, div in TIME_DURATION_UNITS:
+        amount, seconds = divmod(int(seconds), div)
+        if amount > 0:
+            parts.append('{} {}{}'
+                         .format(amount, unit, "" if amount == 1 else "s"))
+    return ', '.join(parts)
+    
+   
+
+@Client.on_message(command("start") & filters.private & ~filters.edited)
+async def start_(client: Client, message: Message):
+    await message.reply_photo(
+        photo=f"https://telegra.ph/file/f323bcaec71ba138fb6df.png",
+        caption=f"""**المطور @libcubehawk .
+ مرحبا بك انا بوت تشغيل الاغاني في المكالمات الصوتيه   ...
+💞 الطريقة في الاسفل 
+الحساب المساعد[𝗔𝘀𝘀𝗶𝘀𝘁𝗮𝗻𝘁 𝗥𝗶𝗻𝗼.♪](https://t.me/Assistant_rino) ...
+**""",
+    reply_markup=InlineKeyboardMarkup(
+            [
+                [
+                    InlineKeyboardButton(
+                        "➕ ❰ اضفني الي مجموعتك ❱ ➕", url=f"https://t.me/{BOT_USERNAME}?startgroup=true"
+                    )
+                ],
+                [
+                    InlineKeyboardButton(
+                        "طريقة التشغيل", url=f"https://t.me/djrino2"
+                    ),
+                    InlineKeyboardButton(
+                        "المطور", url="https://t.me/libcubehawk"
+                    )
+                ],
+                [
+                    InlineKeyboardButton(
+                        "📢 ᴜᴘᴅᴀᴛᴇꜱ ᴄʜᴀɴɴᴇʟ", url=f"https://t.me/{UPDATES_CHANNEL}"
+                    ),
+                    InlineKeyboardButton(
+                        "ꜱᴜᴘᴘᴏʀᴛ ɢʀᴏᴜᴘ 🇮🇳", url=f"https://t.me/{ZAID_SUPPORT}"
+                    )
+                ]
+                
+           ]
+        ),
+    )
+    
+    
+@Client.on_message(commandpro(["/start", "/alive"]) & filters.group & ~filters.edited)
+async def start(client: Client, message: Message):
+    await message.reply_photo(
+        photo=f"https://telegra.ph/file/dd9ca2b2122dd68ffab0e.png",
+        caption=f"""Thanks For Adding Me To Ur Chat, For Any Query U Can Join Our Support Groups 🔥♥️""",
+        reply_markup=InlineKeyboardMarkup(
+            [
+                [
+                    InlineKeyboardButton(
+                        "💥 ᴊᴏɪɴ ʜᴇʀᴇ 💞", url=f"https://t.me/{SUPPORT_GROUP}")
+                ]
+            ]
+        ),
+    )
+
+
+@Client.on_message(command(["repo", "source"]) & filters.group & ~filters.edited)
+async def help(client: Client, message: Message):
+    await message.reply_photo(
+        photo=f"https://telegra.ph/file/92688f2c44a35ba673c23.png",
+        caption=f"""Here Is The Source Code Fork And Give Stars ✨""",
+        reply_markup=InlineKeyboardMarkup(
+            [
+                [
+                    InlineKeyboardButton(
+                        " ʀᴇᴘᴏ ⚒️", url=f"https://replit.com/@dashezup/generate-pyrogram-session-string")
+                ]
+            ]
+        ),
+    )
